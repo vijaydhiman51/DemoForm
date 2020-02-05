8a9e10b796379bbcdc88a1d7d2de4dd62e85b8af
Vijay Dhiman (IND) <VijayD@dev.oati.local>
2020-02-05 04:16:21 -0600

    Add project files.

---
+++ DemoForm/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace DemoForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSerachID = new System.Windows.Forms.TextBox();
+            this.lb_ID = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(94, 34);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(140, 26);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(94, 71);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 26);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLoad.Location = new System.Drawing.Point(46, 129);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(160, 28);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Save Data";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLoadData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadData.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLoadData.Location = new System.Drawing.Point(46, 214);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(160, 28);
+            this.btnLoadData.TabIndex = 5;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = false;
+            this.btnLoadData.Click += new System.EventHandler(this.btnNewForm_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Serach ID";
+            // 
+            // txtSerachID
+            // 
+            this.txtSerachID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSerachID.Location = new System.Drawing.Point(94, 178);
+            this.txtSerachID.Name = "txtSerachID";
+            this.txtSerachID.Size = new System.Drawing.Size(140, 26);
+            this.txtSerachID.TabIndex = 7;
+            // 
+            // lb_ID
+            // 
+            this.lb_ID.AutoSize = true;
+            this.lb_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ID.Location = new System.Drawing.Point(41, 279);
+            this.lb_ID.Name = "lb_ID";
+            this.lb_ID.Size = new System.Drawing.Size(42, 20);
+            this.lb_ID.TabIndex = 8;
+            this.lb_ID.Text = "ID  : ";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_name.Location = new System.Drawing.Point(42, 312);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(59, 20);
+            this.lb_name.TabIndex = 9;
+            this.lb_name.Text = "Name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 353);
+            this.Controls.Add(this.lb_name);
+            this.Controls.Add(this.lb_ID);
+            this.Controls.Add(this.txtSerachID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DB Check";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSerachID;
+        private System.Windows.Forms.Label lb_ID;
+        private System.Windows.Forms.Label lb_name;
+    }
+}
+
---
+++ DemoForm/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DemoForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string con_db = "Data Source=I-VIJAYD\\ENTERPRISE2014;Initial Catalog=SampleDB;Integrated Security=True";
+        SqlConnection con = new SqlConnection(con_db);
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string con_db = "Data Source=I-VIJAYD\\ENTERPRISE2014;Initial Catalog=SampleDB;Integrated Security=True";
+            string ID = txtID.Text;
+            string Name = txtName.Text;
+
+           
+            string query = "insert into SampleDATA values ( " + ID + ", \'" + Name + "\' )";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                 int res =  cmd.ExecuteNonQuery();
+                MessageBox.Show("Record added successfully!! " + res.ToString() );
+            }
+            catch (SqlException abc)
+            {
+                MessageBox.Show(abc.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnNewForm_Click(object sender, EventArgs e)
+        {
+            string ID = txtSerachID.Text;
+            string query = "select ID,Name from SampleDATA where ID = " + ID.ToString();
+            string[] newData = new string[2];
+           
+            
+
+            try
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query,con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        { if (reader.FieldCount != -1) {
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    newData[i] = reader.GetValue(i).ToString();
+
+                                }
+                            }
+
+                            else
+                            {
+                                MessageBox.Show("No record found!!");
+
+                            }
+                        }
+                    }
+                }
+                 
+            }
+            catch (SqlException abc)
+            {
+                MessageBox.Show(abc.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            lb_ID.Show();
+            lb_name.Show();
+
+            lb_ID.Text = "ID : " + newData[0];
+            lb_name.Text = "Name : " + newData[1];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb_ID.Hide();
+            lb_name.Hide();
+            //MessageBox.Show(sender.ToString());
+        }
+    }
+}
